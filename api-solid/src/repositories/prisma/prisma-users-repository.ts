7b038e8458617e993410bd8ce36cb137deb8a936
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-02-19 16:53:34 -0300

    create find user by email interface

--- api-solid/src/repositories/prisma/prisma-users-repository.ts
+++ api-solid/src/repositories/prisma/prisma-users-repository.ts
@@ -4,6 +4,16 @@ import { Prisma } from "@prisma/client";
 import { UserRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements UserRepository {
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email
+      }
+    }) 
+
+    return user
+  }
+
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data, 
